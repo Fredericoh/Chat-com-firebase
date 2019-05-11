88bdf03d8f46fcee889df6447f351853fc6fa263
Fredericoh <fredericoh@gmail.com>
2019-05-11 02:56:53 -0300

    x

--- app/src/main/java/faculdadedelta/edu/br/chatfirebase/RegisterActivity.java
+++ app/src/main/java/faculdadedelta/edu/br/chatfirebase/RegisterActivity.java
@@ -1,19 +1,33 @@
 package faculdadedelta.edu.br.chatfirebase;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.IOException;
+import java.util.UUID;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -21,6 +35,10 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText mEtEmail;
     private EditText mEtSenha;
     private Button mBtnCadastrar;
+    private Button mBtnImagemPerfil;
+    private Uri mSelecionarUri;
+    private ImageView mImgPerfil;
+
 
 
     @Override
@@ -32,6 +50,18 @@ public class RegisterActivity extends AppCompatActivity {
         mEtEmail = findViewById(R.id.etEmail);
         mEtSenha = findViewById(R.id.etSenha);
         mBtnCadastrar = findViewById(R.id.btnCadastrar);
+        mBtnImagemPerfil = findViewById(R.id.btnImagemPerfil);
+        mImgPerfil = findViewById(R.id.imgPerfil);
+
+
+        mBtnImagemPerfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                selecionarImagemPerfil();
+            }
+        });
+
 
         mBtnCadastrar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,6 +75,32 @@ public class RegisterActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 0){
+
+            mSelecionarUri = data.getData();
+
+            Bitmap bitmap = null;
+
+            try {
+               bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), mSelecionarUri);
+               mImgPerfil.setImageDrawable(new BitmapDrawable(bitmap));
+               mBtnImagemPerfil.setAlpha(0);
+            } catch (IOException e) {
+            }
+        }
+    }
+
+    private void selecionarImagemPerfil() {
+
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, 0);
+    }
+
     private void limparCampos(){
         mEtUsuario.setText("");
         mEtEmail.setText("");
@@ -53,12 +109,13 @@ public class RegisterActivity extends AppCompatActivity {
 
     private void criarUsuario(){
 
+        String usuario = mEtUsuario.getText().toString();
         String email = mEtEmail.getText().toString();
         String senha = mEtSenha.getText().toString();
 
-        if (email == null || email.isEmpty() || senha == null || senha.isEmpty()){
+        if (usuario == null || usuario.isEmpty() || email == null || email.isEmpty() || senha == null || senha.isEmpty()){
 
-            Toast.makeText(getBaseContext(), "Os campos email e senha são obrigatorio. ", Toast.LENGTH_LONG).show();
+            Toast.makeText(getBaseContext(), "Os campos usuario, email e senha são obrigatorio. ", Toast.LENGTH_LONG).show();
             return;
         } else {
             Toast.makeText(getBaseContext(), "Usuario cadastrado com sucesso. ", Toast.LENGTH_LONG).show();
@@ -72,6 +129,8 @@ public class RegisterActivity extends AppCompatActivity {
 
                 if (task.isSuccessful()){
                     Log.i("Teste", task.getResult().getUser().getUid());
+
+                    salvarUsuarioNoFireBase();
                 }
             }
         })
@@ -83,5 +142,31 @@ public class RegisterActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void salvarUsuarioNoFireBase() {
+
+        String nomeArquivo = UUID.randomUUID().toString();
+        final StorageReference reference = FirebaseStorage.getInstance().getReference("/images/" + nomeArquivo);
+        reference.putFile(mSelecionarUri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                        reference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+
+                                Log.i("Teste", uri.toString());
+                            }
+                        });
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.i("Teste", e.getMessage(), e);
+                    }
+                });
     }
 }
